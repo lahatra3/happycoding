3196cc8a4b8939ddf66693d78f2be8a3241ec80d
lahatra3 <lahatrap20.aps2a@gmail.com>
2023-05-07 02:45:54 +0300

    animation ...

--- webcup/src/app/app.component.ts
+++ webcup/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  header!: HTMLElement;
   title = 'webcup';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.header = <HTMLElement>document.querySelector('header');
+    window.addEventListener('scroll', () => {
+      this.header.classList.toggle('scrollHeader', window.scrollY > 19);      
+    });
+  }
 }
--- webcup/src/app/home/home.module.ts
+++ webcup/src/app/home/home.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { PartoneComponent } from './components/partone/partone.component';
-import { ParttwoComponent } from './components/parttwo/parttwo.component';
 import { PartthreeComponent } from './components/partthree/partthree.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     PartoneComponent,
-    ParttwoComponent,
     PartthreeComponent
   ],
   imports: [
