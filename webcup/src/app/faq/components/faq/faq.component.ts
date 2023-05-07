71f1ea0e78a7d9785d57b30d5082298a7aaeddf3
Charla19 <charlap20.aps2a@gmail.com>
2023-05-07 08:37:36 +0300

    all avy aty amiko

--- webcup/src/app/faq/components/faq/faq.component.ts
+++ webcup/src/app/faq/components/faq/faq.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent {
+export class FaqComponent implements OnInit {
+  @ViewChild('questionsContainer') questionsContainer!: ElementRef;
+  constructor() {}
 
+  ngOnInit(): void {
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach( button =>{
+        button.addEventListener('click',()=>{
+            const faq = button.nextElementSibling as Element;
+            const icon = button.children[1];
+
+            faq.classList.toggle('show');
+            icon.classList.toggle('rotate');
+
+            // close all other questions
+          const questions = this.questionsContainer.nativeElement.querySelectorAll('.question');
+          questions.forEach((question: any) => {
+            if (question !== faq.parentElement) {
+              question.querySelector('.question-content')?.classList.remove('show');
+              question.querySelector('.d-arrow')?.classList.remove('rotate');
+            }
+          });
+        })
+    } )
+    const a = <HTMLElement>document.querySelector('.robot a');
+    const img = <HTMLElement>document.querySelector('.robot a img');
+
+    img.addEventListener('mouseenter', () => {
+      a.classList.add('hovered');
+    });
+
+    a.addEventListener('mouseleave', () => {
+      a.classList.add('hovered');
+    });
+
+  }
 }
